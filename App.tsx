d0361f93c74ebdba638c3e52830f1c2b0c3cab63
Atul Gupta <atul.gupta@guruq.in>
2024-02-25 15:16:43 +0530

    retrieve user profile after authentacation login and add google firebase

--- App.tsx
+++ App.tsx
@@ -1,81 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { ApolloProvider } from '@apollo/client';
-import { View, TextInput, Button, Text } from 'react-native';
-import { useMutation, gql, ApolloClient, InMemoryCache } from '@apollo/client';
-import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import React from 'react';
+import { Platform } from 'react-native';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider, createTheme,lightColors } from '@rneui/themed';
+import { View } from 'react-native';
+import LoginScreen from './src/screens/Login/LoginScreen';
+import { AuthProvider } from './AuthProvider';
 
-// import SplashScreen from './SplashScreen';
-import SplashScreen from 'react-native-splash-screen'
-
-if (__DEV__) {
-  loadDevMessages();
-  loadErrorMessages();
-}
-
+// Initialize Apollo Client
 const client = new ApolloClient({
   uri: 'https://dev1.classupclient.com/graphql',
   cache: new InMemoryCache()
 });
 
-const LOGIN_MUTATION = gql`
-  mutation TokenAuth($username: String!, $password: String!) {
-    tokenAuth(username: $username, password: $password) {
-      token
-    }
-  }
-`;
-
-const LoginScreen = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const [login, { loading, error, data }] = useMutation(LOGIN_MUTATION);
-
-  const handleLogin = () => {
-    login({ variables: { username, password } });
-  };
-
-  return (
-    <View>
-      <TextInput
-        placeholder="Username or Email"
-        value={username}
-        onChangeText={text => setUsername(text)}
-      />
-      <TextInput
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={text => setPassword(text)}
-      />
-      <Button title="Login" onPress={handleLogin} />
-      {loading && <Text>Loading...</Text>}
-      {error && <Text>Error: {error.message}</Text>}
-      {data && data.tokenAuth.token && <Text>Login successful!</Text>}
-    </View>
-  );
-};
+const theme = createTheme({
+  lightColors: {
+    ...Platform.select({
+      default: lightColors.platform.android,
+      ios: lightColors.platform.ios,
+    })},
+  darkColors: {
+    primary: '#000',
+  },
+  mode: 'light',
+});
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate loading process (e.g., fetching initial data, checking authentication, etc.)
-    setTimeout(() => {
-      setLoading(false); // Set loading to false after a delay (e.g., 2 seconds)
-    }, 2000); // Adjust delay as needed
-  }, []);
-
   return (
     <ApolloProvider client={client}>
-      {loading ? (
-        // <SplashScreen />
-        <LoginScreen />
-      ) : (
-        <View style={{ flex: 1 }}>
-          <LoginScreen />
-        </View>
-      )}
+      <ThemeProvider theme={theme}>
+        <SafeAreaProvider>
+          <AuthProvider>
+            <View style={{ flex: 1 }}>
+              <LoginScreen />
+            </View>
+          </AuthProvider>
+        </SafeAreaProvider>
+      </ThemeProvider>
     </ApolloProvider>
   );
 };
