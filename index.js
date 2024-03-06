77a6cb22b6abece15f71c3239b964715d66a0baa
Atul Gupta <atul.gupta@guruq.in>
2024-03-06 21:12:21 +0530

    slight correction in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import LoginScreen from './App';
+import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => LoginScreen);
+AppRegistry.registerComponent(appName, () => App);
